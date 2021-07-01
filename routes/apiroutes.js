f93527a465952057215672d7fad44dacb986dcd7
Sonika <sonikapoudyal5@gmail.com>
2021-07-01 10:33:42 -0400

    created findbyId function

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,30 +1,26 @@
 const router = require('express').Router();
 const db = require('../db/db.json');
 const fs = require('fs');
-// const findById = require('')
+const {v4: uuidv4} = require('uuid');
 
-router.get('/api/notes', (req,res) => {
-   fs.readFile('./db/db.json', 'utf8', (err,data) => {
-       if(err){
-           throw err
-       }
-       res.json(JSON.parse(data));
-   }) 
-})
 
-// router.get('/api/notes/:id', (req,res) => {
-//     const result = findById(req.params.id, notes);
-//     if(result){
-//         res.json(result);
-//     }else{
-//         res.send(404);
-//     }
-// });
+router.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            throw err
+        }
+        res.json(JSON.parse(data));
+    });
+});
 
-router.post('/api/notes', (req,res) => {
-    req.body.id = notes.length.toString();
-    const notes = createNewNotes(req.body, notes);
-    res.json(notes);
+router.post('/api/notes', (req, res) => {
+    if (!req.body.id) {
+        req.body.id = uuidv4();
+        createNewNotes(req.body, notes);
+    } else {
+        editNote(req.body, notes);
+    }
+    res.json(req.body);
 });
 
 
