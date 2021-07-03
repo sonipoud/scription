e7fb6d1a2a8eb5f8ba0987923dfd5e8aa0027698
Sonika <sonikapoudyal5@gmail.com>
2021-07-02 21:27:24 -0400

    last entry

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -4,20 +4,19 @@ const fs = require("fs");
 const { createNewNote } = require('../lib/notes.js')
 
 
-// router.get('/api/notes', (req, res) => {
-//     let data = 
-//     fs.readFileSync('./db/db.json', 'utf8', (err, data) => {
-//         if (err) {
-//             throw err
-//         }
-//         res.json(data);
-//     });
-// });
+router.get('/api/notes', (req, res) => {
+    let data = JSON.parse(fs.readFileSync('./db/db.json'))
 
-router.get('/api/notes', (req,res) => {
-    let data = notes;
-    res.json(data);
+        res.json(data);
+  
 });
+
+
+
+// router.get('/api/notes', (req,res) => {
+//     let data = notes;
+//     res.json(data);
+// });
 
 router.post("/api/notes", (req, res) => {
     let note = createNewNote(req.body, notes);
